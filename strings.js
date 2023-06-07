a2de2ead8b0a227f127e9ef282928942e9820168
Aamir <aamirasaram2107@gmail.com>
2023-06-07 13:22:44 +0200

    Reverse word function and test

--- strings.js
+++ strings.js
@@ -1,8 +1,12 @@
-const stringLength = (string) => {
+module.exports = {};
+
+module.exports.stringLength = (string) => {
   if (string.length < 1 || string.length > 10) {
     throw new Error('String length must be between 1 and 10');
   }
   return string.length;
 };
 
-module.exports = stringLength;
+module.exports.reverseString = (string) => {
+  return string.split('').reverse().join('');
+}
--- strings.test.js
+++ strings.test.js
@@ -1,17 +1,21 @@
 const strings = require('./strings');
 
 test('length of word is 4', () => {
-  expect(strings('word')).toBe(4);
+  expect(strings.stringLength('word')).toBe(4);
 });
 
 test('length of word is 10', () => {
-  expect(strings('wordwordwo')).toBe(10);
+  expect(strings.stringLength('wordwordwo')).toBe(10);
 });
 
 test('length of word is 11', () => {
-  expect(() => { strings('wordwordwor'); }).toThrow();
+  expect(() => { strings.stringLength('wordwordwor'); }).toThrow();
 });
 
 test('length of word is 0', () => {
-  expect(() => { strings(''); }).toThrow();
+  expect(() => { strings.stringLength(''); }).toThrow();
+});
+
+test('Reverse word', () => {
+  expect(strings.reverseString('word')).toBe('drow');
 });
